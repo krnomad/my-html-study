3ac3fa3fff9f034ed79a64025e84ef658d553315
jong won Kang <krnomad@gmail.com>
2023-02-05 22:01:11 +0900

    chatper2 practics finish

--- chapter2/practics/coordinates/main.js
+++ chapter2/practics/coordinates/main.js
@@ -7,13 +7,21 @@ addEventListener('mousemove', (ev) => {
     const target = document.querySelector('.target')
     const targetRect = target.getBoundingClientRect()
 
+    const positionX = ev.clientX - targetRect.width / 2
+    const positionY = ev.clientY - targetRect.height / 2
+
     // select target element, and change position to mouse position
-    document.querySelector('.target').style.left = ev.clientX-targetRect.width/2 + 'px'
-    document.querySelector('.target').style.top = ev.clientY-targetRect.height/2 + 'px'
+    target.style.left = positionX + 'px'
+    target.style.top = positionY + 'px'
 
     // change the position of the horizontal line to the mouse position
     document.querySelector('.horizontal').style.top = ev.clientY + 'px'
 
     // change the position of the vertical line to the mouse position
     document.querySelector('.vertical').style.left = ev.clientX + 'px'
+
+    const stringTarget = document.querySelector('.stringTarget')
+    stringTarget.textContent = `(${ev.clientX}, ${ev.clientY})`
+    stringTarget.style.left = (ev.clientX) + 'px'
+    stringTarget.style.top = ev.clientY + 'px'
 })
